81f5a1c56f264e3087bf44b6450c011286633bb5
Jin <76438011+KimJinWooDa@users.noreply.github.com>
2024-11-26 20:17:49 +0900

    [feat] balance

--- AIRogueRPG/Assets/Script/PlayerMoveController.cs
+++ AIRogueRPG/Assets/Script/PlayerMoveController.cs
@@ -15,7 +15,7 @@ public class PlayerMoveController : MonoBehaviour
             return instance;
         }
     }
-    private float speed = 3f;
+    [SerializeField] private float speed = 3f;
     private float collisitionOffset = 0.05f;
     private Vector2 movement, attackDir;
     private new Rigidbody2D rigidbody;
